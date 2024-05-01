6b42f5f35f1f4cddb5410934b2754cf6ab26d254
HIMANSHU <HIMANSHU@DESKTOP-J1M9KRJ>
2024-05-01 12:25:48 +0530

    UpdateOPerations

--- JDBCCrudApp/src/in/ineuron/presistence/IStudentDao.java
+++ JDBCCrudApp/src/in/ineuron/presistence/IStudentDao.java
@@ -1,10 +1,12 @@
-package in.ineuron.dao;
+package in.ineuron.presistence;
+
+import in.ineuron.dto.Student;
 
 public interface IStudentDao 
 {
 	public String addStudent(Integer id,String name,Integer age);
 	public Student searchStudent(Integer id);
-	public String updateStudent(Integer id,String name,Integer age);
 	public String deleteStudent(Integer id);
+	public String updateStudent(Student student);
 
 }
