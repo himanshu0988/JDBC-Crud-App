cd9e043cdec08166888a435b8cf15f6857a8ed9f
HIMANSHU <HIMANSHU@DESKTOP-J1M9KRJ>
2024-05-01 12:26:16 +0530

    UpdateOPerations

--- JDBCCrudApp/src/in/ineuron/service/IStudentService.java
+++ JDBCCrudApp/src/in/ineuron/service/IStudentService.java
@@ -7,8 +7,8 @@ public interface IStudentService
 
 	public String addStudent(Integer id,String name,Integer age);
 	public Student searchStudent(Integer id);
-	public String updateStudent(Integer id,String name,Integer age);
 	public String deleteStudent(Integer id);
+	public String updateStudent(Student newStudent);
 
 
 }
--- JDBCCrudApp/src/in/ineuron/service/StudentServiceImpl.java
+++ JDBCCrudApp/src/in/ineuron/service/StudentServiceImpl.java
@@ -1,32 +1,44 @@
 package in.ineuron.service;
 
+import in.ineuron.daofectory.StudentDaofectory;
 import in.ineuron.dto.Student;
+import in.ineuron.presistence.IStudentDao;
+import in.ineuron.servicefectory.StudentServiceFectory;
 
 public class StudentServiceImpl implements IStudentService
 {
 
+	IStudentDao studentdao=null;
 	@Override
 	public String addStudent(Integer id, String name, Integer age) {
-		// TODO Auto-generated method stub
-		return null;
+		if(studentdao==null)
+		{
+			studentdao=StudentDaofectory.getStudentDao();
+		}
+	return studentdao.addStudent(id, name, age);
+		
 	}
 
 	@Override
-	public Student searchStudent(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Student searchStudent(Integer id) 
+	{
+		studentdao=StudentDaofectory.getStudentDao();
+		return studentdao.searchStudent(id);
 	}
 
 	@Override
-	public String updateStudent(Integer id, String name, Integer age) {
-		// TODO Auto-generated method stub
-		return null;
+	public String updateStudent(Student student) {
+		studentdao=StudentDaofectory.getStudentDao();
+		return studentdao.updateStudent(student);
 	}
 
 	@Override
 	public String deleteStudent(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+	if(studentdao==null)
+	{
+		studentdao=StudentDaofectory.getStudentDao();
+	}
+	return studentdao.deleteStudent(id);
 	}
 
 }
